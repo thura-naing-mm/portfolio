bcdf4ea9f21870694f3d56cc20301099f12e887c
mtm-thuranaing <thuranaing@metateammyanmar.com>
2024-04-03 14:55:39 +0630

    add speed insight package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { META } from './constant/index.constants'
 import { Inter } from 'next/font/google'
 import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -34,6 +35,7 @@ export default function RootLayout({
       <body className={inter.className} suppressHydrationWarning={true}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
